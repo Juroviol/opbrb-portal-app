fb3df9951b6a90fecb291e2f292e59300ac029f8
Juliano Rodrigues da Silva <julianorodrigues@flexge.com>
2025-02-01 19:04:37 -0300

    feat: esqueleto inicial criado com o vite

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx", ".json"],
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@features": path.resolve(__dirname, "src/features"),
+    },
+  }
+})
